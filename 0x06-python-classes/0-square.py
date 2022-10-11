405eec7c57a74cd6ad3080d222f6381697e2bb18
Pelumiii1 <adesokanpelumi14@gmail.com>
2022-10-11 11:23:39 +0100

    started python classes tasks

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Filename: 0-square"""
+
+
+class Square:
+    """An empty Square that defines a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Files defines a square"""
+
+
+class Square:
+    """A class Square that defines a square"""
+
+    def __init__(self, size=None):
+        """Initializes private attribute size"""
+        self.__size = size
